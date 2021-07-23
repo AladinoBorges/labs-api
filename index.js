908a0b08b2099528133e64e188a56dd07a03fa05
Aladino Borges <eualadino@gmail.com>
2021-07-23 01:50:24 -0300

    Lógica index.js

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { exams, labs } from './mocks/data.js';
+import labsRoutes from './routes/labs.js';
+import examsRoutes from './routes/exams.js';
 
 const app = express();
 const PORT = 5000;
+const getIds = ((bulkData) => bulkData.map(({ id }) => id));
 
 app.use(bodyParser.json());
+
+app.use('/labs', labsRoutes);
+app.use('/exams', examsRoutes);
+
+app.get('/', (_request, response) => {
+
+  response.send('endpoints: [/labs] [/labs/id] [/exams] [/exams/id]');
+});
 
 app.listen(PORT, () => console.log(`Servidor a funcionar na porta: http://localhost:${PORT}`));
